627f797e56cd363765c5268f91b06bffaf6ccb62
Pau Giuli <pau@giuli.cat>
2023-12-25 19:49:18 +0100

    first commit

---
+++ introduccio/primer.py
@@ -0,0 +1,19 @@
+import turtle
+
+tortuga = turtle.Turtle()
+
+tortuga.goto(0, 0)
+tortuga.pensize(5)
+tortuga.speed(1)
+
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.forward(100)
+tortuga.left(90)
+tortuga.goto(0, 200)
+
+
